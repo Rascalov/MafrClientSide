382a579162e1b8b26543808516788821ade20dda
Rascalov <tim.sanou22@gmail.com>
2020-08-14 01:37:34 +0200

    Removed debugging lines

--- src/main/java/org/mafr/App.java
+++ src/main/java/org/mafr/App.java
@@ -42,22 +42,5 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-        // todo: mafr Server seems to return a decode-able unicode (given you add % per u), but client gives ?????'s all the time.
-        System.out.println("u0627u0635u0648u0644 u0627u0644u062du062fu064au062b");
-        System.out.println("<a href=\"https://moodle.inholland.nl/pluginfile.php/303016/mod_page/intro/u0627u0635u0648u0644 u0627u0644u062du062fu064au062b.docx\" target=\"_blank\" alt=\"\">u0627u0635u0648u0644 u0627u0644u062du062fu064au062b.docx</a>");
-        Document document = Jsoup.parse("<a href=\"https://moodle.inholland.nl/pluginfile.php/303016/mod_page/intro/u0627u0635u0648u0644 u0627u0644u062du062fu064au062b.docx\" target=\"_blank\" alt=\"\">u0627u0635u0648u0644 u0627u0644u062du062fu064au062b.docx</a>");
-        System.out.println(document.html());
-        document = Jsoup.parse("<a href=\\\"https:\\/\\/moodle.inholland.nl\\/pluginfile.php\\/303016\\/mod_page\\/intro\\/\\u0627\\u0635\\u0648\\u0644 \\u0627\\u0644\\u062d\\u062f\\u064a\\u062b.docx\\\" target=\\\"_blank\\\" alt=\\\"\\\">\\u0627\\u0635\\u0648\\u0644 \\u0627\\u0644\\u062d\\u062f\\u064a\\u062b.docx<\\/a>");
-        System.out.println(document.html());
-        System.out.println("اصول الحديث");
-        MoodleAutomater automater = new MoodleAutomater(6161, "https://damp-lowlands-42720.herokuapp.com", "inhollandFree");
-        try {
-            automater.getContentSection(new MoodleFolder("", 11));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        launch();
-    }
+    public static void main(String[] args) { launch(); }
 }
